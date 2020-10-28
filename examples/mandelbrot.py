5fde740a183f70e55d4885c1cf0581b3afab2be2
Jakub Konka <kubkon@golem.network>
2020-10-28 11:51:26 +0100

    Fix pfaas incorrectly generating input args paths
    Finalize mandelbrot example to allow for any number of tasks and
    permit either launching them sequentially or concurrently.

--- examples/mandelbrot.py
+++ examples/mandelbrot.py
@@ -30,20 +30,24 @@ def compute_rectangle(n: int, start_y: int, end_y: int, width: int, height: int)
 
     return (n, output)
 
-async def main(width: int, height: int, in_parallel: int):
-    max_row_size = math.ceil(height / in_parallel)
+async def main(width: int, height: int, tasks: int, in_parallel: int):
+    max_row_size = math.ceil(height / tasks)
 
     chunks = []
-    for n in range(0, in_parallel):
+    for n in range(0, tasks):
         start_y = n * max_row_size
         end_y = height if start_y + max_row_size > height else start_y + max_row_size
         chunks += [(n, start_y, end_y)]
 
+    output = []
     futs = []
-    for chunk in chunks:
+    for (i, chunk) in enumerate(chunks):
         futs += [compute_rectangle(*chunk, width, height)]
+        if (i + 1) % in_parallel == 0:
+            output += await asyncio.gather(*futs)
+            futs = []
 
-    output = await asyncio.gather(*futs)
+    output += await asyncio.gather(*futs)
     output = sorted(output, key=lambda x: x[0])
 
     with open("mandelbrot.png", "wb") as f:
@@ -57,11 +61,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generates a Mandelbrot set and saves as PNG.")
     parser.add_argument("--width", dest="width", default=600, help="Width of the image to generate.")
     parser.add_argument("--height", dest="height", default=400, help="Height of the image to generate.")
-    parser.add_argument("--in_parallel", dest="in_parallel", default=4, help="Maximum number of parallel computations to run.")
+    parser.add_argument("--tasks", dest="tasks", default=4, help="Maximum number of tasks to create.")
+    parser.add_argument("--in_parallel", dest="in_parallel", default=1, help="Maximum number of parallel computations to run.")
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
-    task = loop.create_task(main(int(args.width), int(args.height), int(args.in_parallel)))
+    task = loop.create_task(main(int(args.width), int(args.height), int(args.tasks), int(args.in_parallel)))
     
     try:
         asyncio.get_event_loop().run_until_complete(task)
