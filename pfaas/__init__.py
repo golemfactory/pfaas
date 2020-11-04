c67f0f9e29ee350d08cfa069885187a44f470ce4
Jakub Konka <kubkon@golem.network>
2020-11-04 17:33:44 +0100

    Use executor pool for local concurrent execution

--- pfaas/__init__.py
+++ pfaas/__init__.py
@@ -1,8 +1,22 @@
 import json
 from datetime import timedelta
-import marshal
 import tempfile
 from pathlib import PurePath
+import marshal
+
+
+def _local_remote_fn(module_path, *args):
+    import marshal
+    import types
+
+    # Load func from file
+    with open(module_path, "rb") as f:
+        code = marshal.load(f)
+
+    # Invoke
+    func = types.FunctionType(code, globals(), "remote")
+    return func(*args)
+
 
 class remote_fn:
     def __init__(
@@ -11,11 +25,20 @@ class remote_fn:
             budget: float = 100.0,
             timeout: timedelta = timedelta(minutes=10),
             subnet: str = "devnet-alpha.2",
+            max_workers: int = 2,
         ):
         self.run_local = run_local
         self.budget = budget
         self.timeout = timeout
         self.subnet = subnet
+
+        if run_local:
+            from concurrent.futures import ProcessPoolExecutor
+            self.engine = ProcessPoolExecutor(max_workers=max_workers)
+
+    def __del__(self):
+        if self.run_local:
+            self.engine.shutdown()
 
     def __call__(self, func):
         async def inner(*args, **kwargs):
@@ -25,36 +48,25 @@ class remote_fn:
             with open(module_path, "wb") as f:
                 marshal.dump(func.__code__, f)
 
-            # Save input args to files
-            saved_args = []
-            for i, arg in enumerate(args):
-                arg_path = PurePath(f"{tmpdir.name}/arg{i}")
-                with open(arg_path, "w") as f:
-                    json.dump(arg, f)
-                saved_args.append(arg_path)
-
             if self.run_local:
-                import types
+                import asyncio
 
-                # Load func from file
-                with open(module_path, "rb") as f:
-                    code = marshal.load(f)
-
-                # Load input args
-                parsed_args = []
-                for arg_path in saved_args:
-                    with open(arg_path, "r") as f:
-                        arg = json.load(f)
-                        parsed_args.append(arg)
-
-                # Invoke
-                deser = types.FunctionType(code, globals(), "remote")
-                return deser(*parsed_args)
+                fut = self.engine.submit(_local_remote_fn, module_path, *args)
+                res = await asyncio.wait_for(asyncio.wrap_future(fut), self.timeout.seconds)
+                return res
 
             else:
                 from yapapi.runner import Engine, Task, vm
                 from yapapi.runner.ctx import WorkContext
                 from yapapi.log import enable_default_logger, log_summary
+
+                # Save input args to files
+                saved_args = []
+                for i, arg in enumerate(args):
+                    arg_path = PurePath(f"{tmpdir.name}/arg{i}")
+                    with open(arg_path, "w") as f:
+                        json.dump(arg, f)
+                    saved_args.append(arg_path)
 
                 enable_default_logger()
                 package = await vm.repo(
